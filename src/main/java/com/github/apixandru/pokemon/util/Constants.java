f7d38b26e8af03db36c7e18ddf0691e43312f02a
Alexandru Bledea <admin@notbed.com>
2015-06-05 18:29:02 +0300

    add constants

---
+++ src/main/java/com/github/apixandru/pokemon/util/Constants.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package com.github.apixandru.pokemon.util;
+
+/**
+ * @author Alexandru Bledea
+ * @since Jun 5, 2015
+ */
+public interface Constants {
+
+	byte DIRECTION_UP = 0;
+	byte DIRECTION_RIGHT = 1;
+	byte DIRECTION_DOWN = 2;
+	byte DIRECTION_LEFT = 3;
+
+	byte POS_X = 0;
+	byte POS_Y = 1;
+
+	byte[][] DIRECTION_MODIFIERS = {
+			{+0, -1},
+			{+1, +0},
+			{+0, +1},
+			{-1, +0},
+	};
+
+}
