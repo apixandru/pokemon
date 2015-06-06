5c84a9050283c0f02f47b526d1d9ebadf24392fc
Alexandru Bledea <admin@notbed.com>
2015-06-06 11:38:53 +0300

    load more stuff

--- src/main/java/com/github/apixandru/pokemon/ui/util/sprites/CharacterSprites.java
+++ src/main/java/com/github/apixandru/pokemon/ui/util/sprites/CharacterSprites.java
@@ -3,6 +3,9 @@
  */
 package com.github.apixandru.pokemon.ui.util.sprites;
 
+import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.MOVING_DOWN;
+import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.MOVING_LEFT;
+import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.MOVING_UP;
 import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.SITTING_DOWN;
 import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.SITTING_LEFT;
 import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.SITTING_UP;
@@ -10,6 +13,7 @@ import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.SITTI
 import java.util.Arrays;
 import java.util.List;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
@@ -21,16 +25,45 @@ import org.newdawn.slick.SpriteSheet;
 public final class CharacterSprites {
 
 	public final List<Image> notMoving;
+	public final List<Animation> moving;
 
 	/**
 	 * @param spriteSheet
 	 */
 	private CharacterSprites(final SpriteSheet spriteSheet) {
+		final Image sittingUp = spriteSheet.getSprite(0, SITTING_UP);
+		final Image sittingLeft = spriteSheet.getSprite(0, SITTING_LEFT);
+		final Image sittingRight = sittingLeft.getFlippedCopy(true, false);
+		final Image sittingDown = spriteSheet.getSprite(0, SITTING_DOWN);
+
+		final Image moveUp = spriteSheet.getSprite(0, MOVING_UP);
+		final Image moveLeft = spriteSheet.getSprite(0, MOVING_LEFT);
+		final Image moveRight = moveLeft.getFlippedCopy(true, false);
+		final Image moveDown = spriteSheet.getSprite(0, MOVING_DOWN);
+
 		this.notMoving = Arrays.asList(
-				spriteSheet.getSprite(0, SITTING_UP),
-				spriteSheet.getSprite(0, SITTING_LEFT).getFlippedCopy(true, false),
-				spriteSheet.getSprite(0, SITTING_DOWN),
-				spriteSheet.getSprite(0, SITTING_LEFT));
+				sittingUp,
+				sittingRight,
+				sittingDown,
+				sittingLeft);
+
+
+		this.moving = Arrays.asList(animation(moveUp, sittingUp),
+				animation(moveLeft, sittingLeft),
+				animation(moveDown, sittingDown),
+				animation(moveRight, sittingRight));
+	}
+
+	/**
+	 * @param frameMoving
+	 * @param frameSitting
+	 * @return
+	 */
+	private static Animation animation(final Image frameMoving, final Image frameSitting) {
+		final Image[] image = {frameMoving, frameSitting, frameMoving.getFlippedCopy(true, false), frameSitting};
+		final Animation animation = new Animation(image, 500);
+		animation.setAutoUpdate(false);
+		return animation;
 	}
 
 	/**
