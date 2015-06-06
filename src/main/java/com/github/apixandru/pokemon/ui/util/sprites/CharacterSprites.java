28662f8bf2e0d8d18e7d5a9f0ba32c9740f9d2de
Alexandru Bledea <admin@notbed.com>
2015-06-06 11:04:29 +0300

    load sprites

--- src/main/java/com/github/apixandru/pokemon/ui/util/sprites/CharacterSprites.java
+++ src/main/java/com/github/apixandru/pokemon/ui/util/sprites/CharacterSprites.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 
 /**
@@ -24,12 +25,20 @@ public final class CharacterSprites {
 	/**
 	 * @param spriteSheet
 	 */
-	public CharacterSprites(final SpriteSheet spriteSheet) {
+	private CharacterSprites(final SpriteSheet spriteSheet) {
 		this.notMoving = Arrays.asList(
 				spriteSheet.getSprite(0, SITTING_UP),
 				spriteSheet.getSprite(0, SITTING_LEFT).getFlippedCopy(true, false),
 				spriteSheet.getSprite(0, SITTING_DOWN),
 				spriteSheet.getSprite(0, SITTING_LEFT));
+	}
+
+	/**
+	 * @throws SlickException
+	 */
+	public static CharacterSprites load(final String image) throws SlickException {
+		final SpriteSheet spriteSheet = SpriteParser.loadSpriteSheet(image);
+		return new CharacterSprites(spriteSheet);
 	}
 
 }
--- src/main/java/com/github/apixandru/pokemon/ui/util/sprites/SpriteParser.java
+++ src/main/java/com/github/apixandru/pokemon/ui/util/sprites/SpriteParser.java
@@ -6,6 +6,7 @@ package com.github.apixandru.pokemon.ui.util.sprites;
 import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.SPRITE_HEIGHT;
 import static com.github.apixandru.pokemon.ui.util.sprites.SpriteConstants.SPRITE_WIDTH;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
@@ -16,14 +17,13 @@ import org.newdawn.slick.SpriteSheet;
  */
 public class SpriteParser {
 
-
 	/**
 	 * @param resource
 	 * @return
 	 * @throws SlickException
 	 */
 	public static SpriteSheet loadSpriteSheet(final String resource) throws SlickException {
-		final Image image = new Image(resource);
+		final Image image = new Image(resource, Color.white);
 		image.setFilter(Image.FILTER_NEAREST);
 		return new SpriteSheet(image, SPRITE_WIDTH, SPRITE_HEIGHT);
 
