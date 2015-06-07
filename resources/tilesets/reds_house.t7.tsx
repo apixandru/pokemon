e0621a4ca40d5cfe4b49cbe61900595bc63de63c
Alexandru Bledea <admin@notbed.com>
2015-06-07 10:36:20 +0300

    add resources

---
+++ resources/tilesets/reds_house.t7.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="reds_house.t7" tilewidth="8" tileheight="8">
+ <image source="reds_house.t7.png" width="128" height="40"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="-1"/>
+ </terraintypes>
+</tileset>
