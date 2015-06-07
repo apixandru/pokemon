0df8b0372a1bfe0c6e6c5f4ba996bbfbf5073a1b
Alexandru Bledea <admin@notbed.com>
2015-06-07 10:37:39 +0300

    add actual map

--- src/main/java/com/github/apixandru/pokemon/ui/GameMap.java
+++ src/main/java/com/github/apixandru/pokemon/ui/GameMap.java
@@ -6,7 +6,10 @@ package com.github.apixandru.pokemon.ui;
 import static com.github.apixandru.pokemon.util.Constants.BLOCK_HEIGHT;
 import static com.github.apixandru.pokemon.util.Constants.BLOCK_WIDTH;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
 
 import com.github.apixandru.pokemon.ui.util.CanRender;
 
@@ -16,16 +19,30 @@ import com.github.apixandru.pokemon.ui.util.CanRender;
  */
 public class GameMap implements CanRender {
 
+	private final TiledMap actualMap;
+
+	/**
+	 * @throws SlickException
+	 */
+	public GameMap() throws SlickException {
+		this.actualMap = new TiledMap("resources/maps/ash_house.tmx");
+	}
+
 	/* (non-Javadoc)
 	 * @see com.github.apixandru.pokemon.ui.util.CanRender#render(org.newdawn.slick.Graphics)
 	 */
 	@Override
 	public void render(final Graphics g) {
+		g.scale(3, 3);
+		actualMap.render(0, 0, 0, 0, 16, 16);
+		g.resetTransform();
+		g.setColor(Color.darkGray);
 		for (int x = 0; x < 10; x++) {
 			for (int y = 0; y < 10; y++) {
 				g.drawRect(x * BLOCK_WIDTH, y * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT);
 			}
 		}
+		g.resetTransform();
 	}
 
 }
