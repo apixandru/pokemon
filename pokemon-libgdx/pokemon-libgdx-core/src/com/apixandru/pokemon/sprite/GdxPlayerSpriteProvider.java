ef21cf60c0b1b29bd60ff8b0790bce1ff0470c58
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-06-23 06:59:44 +0300

    create gdx sprite provider

---
+++ pokemon-libgdx/pokemon-libgdx-core/src/com/apixandru/pokemon/sprite/GdxPlayerSpriteProvider.java
@@ -0,0 +1,37 @@
+package com.apixandru.pokemon.sprite;
+
+import com.apixandru.libgdx.util.Animation;
+import com.apixandru.pokemon.model.Constants;
+import com.apixandru.pokemon.ui.PlayerSpriteProvider;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * @author Alexandru-Constantin Bledea
+ * @since Jun 22, 2016
+ */
+public class GdxPlayerSpriteProvider implements PlayerSpriteProvider<TextureRegion, Animation> {
+
+    private final CharacterSprites sprites;
+
+    public GdxPlayerSpriteProvider(final CharacterSprites sprites) {
+        this.sprites = sprites;
+    }
+
+    @Override
+    public Animation getMoving(Constants.MoveDirection moveDirection) {
+        // TODO remove ordinal() implementation detail
+        return sprites.moving.get(moveDirection.ordinal());
+    }
+
+    @Override
+    public TextureRegion getStanding(Constants.MoveDirection moveDirection) {
+        // TODO remove ordinal() implementation detail
+        return sprites.notMoving.get(moveDirection.ordinal());
+    }
+
+    @Override
+    public void reset() {
+        sprites.reset();
+    }
+
+}
