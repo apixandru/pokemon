c0a430ee780aed57c6b32bb79d34c72c9bd7ee0a
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-06-26 08:45:07 +0300

    more restrictions

--- pokemon-ui/src/com/apixandru/pokemon/ui/PlayerSpriteProvider.java
+++ pokemon-ui/src/com/apixandru/pokemon/ui/PlayerSpriteProvider.java
@@ -2,12 +2,13 @@ package com.apixandru.pokemon.ui;
 
 import com.apixandru.pokemon.model.Constants.MoveDirection;
 import com.apixandru.pokemon.ui.render.Animation;
+import com.apixandru.pokemon.ui.render.Image;
 
 /**
  * @author Alexandru-Constantin Bledea
  * @since Jun 19, 2016
  */
-public interface PlayerSpriteProvider<S, M extends Animation<?, ?>> {
+public interface PlayerSpriteProvider<S extends Image<?>, M extends Animation<?, ?>> {
 
     M getMoving(MoveDirection moveDirection);
 
